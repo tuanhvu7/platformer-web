7da15086264f1592c06ca9949b5405934d9e9ea8
tuanhvu7 <ev.lunar@gmail.com>
2020-03-29 19:43:51 -0700

    Add webpack and babel

--- src/platformer.ts
+++ src/platformer.ts
@@ -1,8 +1,10 @@
 import * as p5 from 'p5';
+import { CONSTANTS } from './const/constants';
 
 function setup() {
+  createCanvas(CONSTANTS.SCREEN_WIDTH, CONSTANTS.SCREEN_HEIGHT);
 }
 
-function draw()  {
+function draw() {
   ellipse(50, 50, 80, 80);
 }
