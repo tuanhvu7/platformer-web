c43285416bec4440641b2f7bfe6f28902e6bacba
tuanhvu7 <ev.lunar@gmail.com>
2020-05-02 11:45:15 -0700

    Key and mouse events working; setTimeout working

---
+++ src/drawable/mouse-controllable.interface.ts
@@ -0,0 +1,6 @@
+/**
+ * Required methods for classes with mouse controls
+ */
+export interface IMouseControllable {
+  mouseClicked(event: MouseEvent): void;
+}
--- src/platformer.ts
+++ src/platformer.ts
@@ -2,13 +2,21 @@ import * as p5 from 'p5';
 import { Constants } from './const/constants';
 import { ResourceUtils } from './utils/resource-utils';
 import { IDrawable } from './drawable/drawable.interface';
+import { IKeyControllable } from './drawable/key-controllable.interface';
+import { IMouseControllable } from './drawable/mouse-controllable.interface';
+import { MockMenu } from './drawable/menus/mock-menu';
 
 /**
  * Contains controls for running app
  */
 class Platformer {
-  public drawable: IDrawable[] = [];
+  private allDrawables: Set<IDrawable> = new Set();
+  private allKeyControllables: Set<IKeyControllable> = new Set();
+  private allMouseControllables: Set<IMouseControllable> = new Set();
 
+  /**
+   * For p5.js
+   */
   public sketch = (mainSketch: p5): void => {
     mainSketch.preload = () => {
       ResourceUtils.DEFAULT_MENU_IMAGE = mainSketch.loadImage(ResourceUtils.DEFAULT_MENU_IMAGE_PATH);
@@ -16,13 +24,67 @@ class Platformer {
 
     mainSketch.setup = () => {
       mainSketch.createCanvas(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+      new MockMenu();
     };
 
     mainSketch.draw = () => {
-      this.drawable.forEach((curDrawable: IDrawable) => {
+      this.allDrawables.forEach((curDrawable: IDrawable) => {
         curDrawable.draw();
       });
     }
+
+    mainSketch.keyPressed = () => {
+      this.allKeyControllables.forEach((curKeyControllable: IKeyControllable) => {
+        curKeyControllable.keyPressed();
+      });
+    }
+
+    mainSketch.mouseClicked = (event: MouseEvent) => {
+      this.allMouseControllables.forEach((curMouseControllable: IMouseControllable) => {
+        curMouseControllable.mouseClicked(event);
+      });
+    }
+  }
+
+/** Getters, setters, and field modifiers **/
+  
+  // Drawables
+  public getAllDrawables(): Set<IDrawable> {
+    return this.allDrawables;
+  }
+
+  public addToAllDrawables(drawable: IDrawable): void {
+    this.allDrawables.add(drawable);
+  }
+
+  public deleteFromAllDrawables(drawable: IDrawable): void {
+    this.allDrawables.delete(drawable);
+  }
+
+  // Key Controllables
+  public getAllKeyControllables(): Set<IKeyControllable> {
+    return this.allKeyControllables;
+  }
+
+  public addToAllKeyControllables(keyControllable: IKeyControllable): void {
+    this.allKeyControllables.add(keyControllable);
+  }
+
+  public deleteFromAllKeyControllables(keyControllable: IKeyControllable): void {
+    this.allKeyControllables.delete(keyControllable);
+  }
+
+  // Mouse Controllables
+  public getAllMouseControllables(): Set<IMouseControllable> {
+    return this.allMouseControllables;
+  }
+
+  public addToAllMouseControllables(mouseControllable: IMouseControllable): void {
+    this.allMouseControllables.add(mouseControllable);
+  }
+
+  public deleteFromAllMouseControllables(mouseControllable: IMouseControllable): void {
+    this.allMouseControllables.delete(mouseControllable);
   }
 }
 
