5bfa79e4a25c5e01732e4d5f906aa01bf7f60d5a
tuanhvu7 <ev.lunar@gmail.com>
2020-05-09 16:53:16 -0700

    Temporary hard-code fix for level 1 big enemy trigger

--- src/drawable/menus/panels/pause-menu-panel.ts
+++ src/drawable/menus/panels/pause-menu-panel.ts
@@ -41,6 +41,7 @@ export class PauseMenuPanel extends APanel {
       platformer.getCurrentActiveLevel().closePauseMenu();
       platformer.getCurrentActiveLevel().deactivateLevel();
       platformer.setCurrentActiveLevelNumber(0);
+      ResourceUtils.loopSong(ESongType.OUT_OF_LEVEL_MENU);
       platformer.getLevelSelectMenu().setupActivateMenu();
     }
 
