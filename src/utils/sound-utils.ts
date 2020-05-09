2f969a7e4997e7e04ae8248d4f2c86f1284ccc74
tuanhvu7 <ev.lunar@gmail.com>
2020-05-09 12:47:41 -0700

    Sound restart working; changed some properties to readonly on constants.ts and resource-utils.ts

--- src/utils/sound-utils.ts
+++ src/utils/sound-utils.ts
@@ -13,7 +13,7 @@ export class SoundUtils {
   /**
    * @param path path of sound file to fetch
    */
-  public static async getSoundFile(path: string): Promise<AudioBufferSourceNode> {
+  public static async getSoundFile(path: string): Promise<AudioBuffer> {
     const headers = new Headers();
     headers.append('Content-Type', 'application/octet-stream');
     const soundFile = await fetch(path, {
@@ -21,17 +21,24 @@ export class SoundUtils {
       headers: headers
     });
 
-    const audioBuffer = await this.audioContext.decodeAudioData((await soundFile.arrayBuffer()));
+    return await this.audioContext.decodeAudioData((await soundFile.arrayBuffer()));
+  }
+
+  /**
+   * @return new AudioBufferSourceNode with buffer set to given audioBuffer
+   * used to replay songs
+   */
+  public static createAudioBufferSourceNode(audioBuffer: AudioBuffer): AudioBufferSourceNode {
     const source = this.audioContext.createBufferSource();
     source.buffer = audioBuffer;
-    source.connect(this.audioContext.destination);
     return source;
   }
 
   /**
    * @param audioBufferSourceNode audio buffer source node to loop
    */
-  public static loopSong(audioBufferSourceNode: AudioBufferSourceNode) {
+  public static loopSong(audioBufferSourceNode: AudioBufferSourceNode): void {
+    audioBufferSourceNode.connect(this.audioContext.destination);
     audioBufferSourceNode.loop = true;
     audioBufferSourceNode.start();
     console.log(audioBufferSourceNode.buffer.duration);
@@ -40,8 +47,9 @@ export class SoundUtils {
   /**
    * @param audioBufferSourceNode audio buffer source node to play
    */
-  public static playSong(audioBufferSourceNode: AudioBufferSourceNode) {
-    audioBufferSourceNode.loop = false;
+  public static playSong(audioBufferSourceNode: AudioBufferSourceNode): void {
+    audioBufferSourceNode.connect(this.audioContext.destination);
+    audioBufferSourceNode.loop = true;
     audioBufferSourceNode.start();
     console.log(audioBufferSourceNode.buffer.duration);
   }
